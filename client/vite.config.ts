c08544cb0ae1b3b3eaffcbae45e53262c32d6496
Miles Eng <miles.eng@duke.edu>
2024-02-19 19:15:52 -0500

    added backend API as proxy

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5001',
+        target: 'https://pantry-pal-lvc7.onrender.com',
         changeOrigin: true
       }
     }
